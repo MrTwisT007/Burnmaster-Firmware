5f89d6e35fdd2ed121e5c1a4dfbd555d2eaae36d
MrTwisT007 <kindiakov@gmail.com>
2026-06-30 15:23:07 +0100

    Deleting old Debug artefacts

--- CartReaderApp/Output/Debug/Obj/GDCartReader/ffunicode.d
+++
@@ -1,9 +0,0 @@
-Output/Debug/Obj/GDCartReader/ffunicode.o: \
-  I:\GZCSir\gbx_cart\cartflasher\CartReaderApp\fatfs\ffunicode.c \
-  I:\GZCSir\gbx_cart\cartflasher\CartReaderApp\fatfs\ff.h \
-  I:\GZCSir\gbx_cart\cartflasher\CartReaderApp\fatfs\ffconf.h \
-  J:\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 6.22a\include\stdint.h \
-  J:\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 6.22a\include\__SEGGER_RTL.h \
-  J:\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 6.22a\include\__SEGGER_RTL_ConfDefaults.h \
-  J:\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 6.22a\include\__SEGGER_RTL_Conf.h \
-  J:\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 6.22a\include\__SEGGER_RTL_Arm_Conf.h
